8483dc4f8232f2e6b17b7a6a7f3dfc9c3d3d2c0d
Estaruncix <mullortomasd@gmail.com>
2026-03-12 15:25:33 +0100

    corrected ending ; omisions

--- queries/top_10_selling_products.sql
+++ queries/top_10_selling_products.sql
@@ -1,5 +1,4 @@
 SELECT product_name, SUM(sales) AS total_sales
 FROM sales
 GROUP BY product_name
-ORDER BY total_sales DESC
-LIMIT 10
+ORDER BY total_sales DESC;
--- queries/top_customers.sql
+++ queries/top_customers.sql
@@ -1,4 +1,4 @@
 SELECT customer_name, SUM(sales) AS total_spent
 FROM sales
 GROUP BY customer_name
-ORDER BY total_spent DESC
+ORDER BY total_spent DESC;
